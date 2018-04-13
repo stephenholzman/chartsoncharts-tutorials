59644a5fc312b5054075b6f599fe85efbb82e259
StephenHolzman <stephen@holzmansmith.com>
2018-04-13 08:45:05 -0400

    Cleaning Fix
    On testing other elections, needed to build out how to handle a broader
    range of expected responses.

--- 2018-04-11-openFEC/analysis.R
+++ 2018-04-11-openFEC/analysis.R
@@ -43,8 +43,10 @@ candidates_running_df <- data.frame(
   party = map(candidates_running[["content"]][["results"]], function(x) x$party) %>% unlist(),
   candidate_status = map(candidates_running[["content"]][["results"]], function(x) x$candidate_status) %>% unlist()
 ) %>%
-  separate(name, c('last_name', 'first_name'), sep = ', ') %>%
-  filter(candidate_status %in% election_of_interest$candidate_status)
+  filter(candidate_status %in% election_of_interest$candidate_status) %>%
+  separate(name, c('last_name', 'first_name'), sep = ', ', extra = "drop", fill = "right") %>%
+  mutate(first_name = if_else(is.na(first_name),"",first_name),
+         full_name = if_else(first_name == "", last_name, paste0(first_name," ",last_name)))
 
 financial_summary <- get_election_financial_summary(api_key,
                                                     query_params = list(
@@ -63,26 +65,44 @@ financial_summary_df <- data.frame(
 ) %>%
   left_join(candidates_running_df, by = c("candidate_id"="candidate_id")) %>%
   filter(candidate_status %in% election_of_interest$candidate_status) %>%
-  select(last_name,candidate_id,party,total_receipts,cash_on_hand_end_period,total_disbursements) %>%
+  select(full_name,candidate_id,party,total_receipts,cash_on_hand_end_period,total_disbursements) %>%
   gather(type,amount,total_receipts:total_disbursements) %>%
   mutate(type = str_replace_all(type,"_"," ") %>% str_to_title,
-         last_name = str_to_title(last_name))
+         full_name = str_to_title(full_name))
 
+period_ending_check <- financial_summary
 
+for(i in 1:length(period_ending_check[["content"]][["results"]])){
+  if(is.null(period_ending_check[["content"]][["results"]][[i]][["coverage_end_date"]])){
+    period_ending_check[["content"]][["results"]][[i]][["coverage_end_date"]] <- "None"
+  }
+}
 
-period_ending <- as.factor(map(financial_summary[["content"]][["results"]], function(x) x$coverage_end_date) %>% unlist())
+period_ending <- data.frame(
+  coverage_end_date = as.factor(map(period_ending_check[["content"]][["results"]], function(x) x$coverage_end_date) %>% unlist()),
+  candidate_name =   map(period_ending_check[["content"]][["results"]], function(x) x$candidate_name) %>% unlist()
+) %>%
+  filter(coverage_end_date != "None")
 
-if(length(levels(period_ending)) != 1){
-  stop("There are different period_endings for the candidates returned.")
+if(length(levels(period_ending)) == 2){
+  
+  subtitle_string <- paste0("Differing period endings: ",substr(levels(period_ending$coverage_end_date)[1],1,10)," and ",substr(levels(period_ending$coverage_end_date)[2],1,10))
+  
+}else if(length(levels(period_ending)) > 2){
+  
+  subtitle_string <- "Many period endings, investigate more"
+  
 }else{
-  period_ending <- substr(levels(period_ending)[1],1,10)
+  
+  subtitle_string <- paste0("Period ending ",substr(levels(period_ending$coverage_end_date)[1],1,10))
+
 }
 
 candidate_order <- financial_summary_df %>%
   filter(type == "Total Receipts") %>%
   arrange(amount)
 
-financial_summary_df$last_name <- factor(financial_summary_df$last_name, levels = candidate_order$last_name)
+financial_summary_df$full_name <- factor(financial_summary_df$full_name, levels = candidate_order$full_name)
 
 colors <- c("DEM" = "#2c4d82", "REP" = "#8e1b1b", "IND" = "#a3a3a3", "GRE" = "#1c561d", "LIB" = "#afac3d")
 
@@ -90,21 +110,21 @@ output_path <- file.path("output",election_of_interest$cycle,election_of_interes
 
 dir.create(output_path,recursive = TRUE)
 
-filename <- paste0(Sys.Date(),"-total-receipts-",election_of_interest$cycle,"-",election_of_interest$state,"-",election_of_interest$office_full,"-",election_of_interest$district,".png")
+filename <- paste0(Sys.Date(),"-financial-summary-",election_of_interest$cycle,"-",election_of_interest$state,"-",election_of_interest$office_full,"-",election_of_interest$district,".png")
 
 author <- "@StephenHolz"
 
 p1 <- financial_summary_df %>%
   ggplot() +
-  geom_bar(aes(x = last_name, y = amount, fill = party), stat = "identity", alpha = .95) +
-  geom_label(aes(x = last_name, y = amount, label = scales::dollar(amount)),hjust = -0.1, label.size = 0, label.padding = unit(0.1,"line")) +
+  geom_bar(aes(x = full_name, y = amount, fill = party), stat = "identity", alpha = .95) +
+  geom_label(aes(x = full_name, y = amount, label = scales::dollar(amount)),hjust = -0.1, label.size = 0, label.padding = unit(0.1,"line")) +
   facet_wrap(~type) +
   coord_flip() +
-  scale_y_continuous(labels = scales::dollar,limits = c(0,max(financial_summary_df$amount)*2)) +
+  scale_y_continuous(labels = scales::dollar,limits = c(if_else(min(financial_summary_df$amount) < 0, min(financial_summary_df$amount), 0),max(financial_summary_df$amount)*2)) +
   scale_fill_manual(values = colors) +
   labs(
     title = paste0(election_of_interest$cycle," ",election_of_interest$state,election_of_interest$district," ",str_to_title(election_of_interest$office_full)," Race Financial Summary"),
-    subtitle = paste0("Period ending ",period_ending),
+    subtitle = subtitle_string,
     x = "",
     y = "",
     caption = paste0("Source: Federal Election Commmission, data retreived on ",Sys.Date(), " by ",author),
@@ -119,7 +139,7 @@ p1 <- financial_summary_df %>%
     axis.ticks = element_blank(),
     plot.title = element_text(size = 26),
     plot.subtitle = element_text(size = 18),
-    axis.text = element_text(size = 10)
+    axis.text = element_text(size = 8)
   )
 
 ggsave(filename = filename, plot = p1, path = output_path, width = 12, height = 6, unit = "in")
